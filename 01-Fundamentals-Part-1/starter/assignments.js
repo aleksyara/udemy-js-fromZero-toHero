82a361fe72862fe7eb9d5d0b5758b927543fac9c
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-08 11:02:02 -0800

    complete lecture 10 and add assignment file

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+// 1. Declarevariablescalled'country','continent'and'population'and assign their values according to your own country (population in millions)
+// 2. Logtheirvaluestotheconsole
+
+let country = "USA";
+let continent = "North America";
+let population = 330;
+
+console.log(country, continent, population + " mln people");
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
 let js = "amazing";
-if (js === "amazing") alert ("JS is FUN!");
+
 console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
