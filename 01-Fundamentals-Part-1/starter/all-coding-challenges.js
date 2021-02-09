34d3fb26258102ca67a02f279aa4ed7fa9ebb082
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-08 19:51:20 -0800

    cimplete Challange 4

--- 01-Fundamentals-Part-1/starter/all-coding-challenges.js
+++ 01-Fundamentals-Part-1/starter/all-coding-challenges.js
@@ -91,15 +91,27 @@ if(averageDolphinsScore > averageKoalasScore && averageDolphinsScore >= minimumS
 
 // Coding Challenge #4
 
-// Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. 
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. 
+// In his country, it's usual to tip 15% if the bill value is between 50 and 300. 
 // If the value is different, the tip is 20%.
 
-// Task 1 1. Calculatethetip,dependingonthebillvalue.Createavariablecalled'tip'for this. 
+// Task 1 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for this. 
 // It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+let bill = 275;
+let tip = null;
+
+// if (bill >= 50 && bill <= 300) {
+//     tip = bill * 0.15
+// } else {
+//     tip = bill * 0.20
+// }
+(bill >= 50 && bill <= 300) ?  tip = bill * 0.15 : tip = bill * 0.20
+console.log("This is a Tip", tip);
 
 // Task 2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). 
 // Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
 
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value will ${bill + tip}`);
 
 // Test data:
 // § Data 1: Test for bill values 275, 40 and 430 
