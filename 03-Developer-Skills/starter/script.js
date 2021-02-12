3afff9b49f6c58e06e26bdc3235987f39db81afc
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-11 16:26:41 -0800

    complete 2 tasks and calculate Temp Amplitude from 1 or 2 arrays

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -34,4 +34,51 @@ console.log(printForecast(weatherFrcstArr));
 
 // console.log('this is a printFrcstArr', weatherFrcstArr);
 
+console.log('PROBLEM 1:');
+
+// We work for a company building a smart home thermometer. 
+// Our most recent task is this: "Given an array of temperatures of one day, 
+// calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+let calcTempAmplitude = function(arr){
+    let maxTemp = 0;
+    let minTemp = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] > maxTemp){
+            // console.log('maxTemp', maxTemp);
+            // console.log('arr[i] in the 1st if', arr[i]);
+            maxTemp = arr[i];
+        } else if (arr[i] < minTemp) {
+            // console.log('arr[i] in the else if', arr[i]);
+            minTemp = arr[i];
+            console.log('minTemp ', minTemp);
+        }
+    }
+    
+    console.log('FINAL max', maxTemp);
+    console.log('FINAL min', minTemp);
+
+    return  console.log('THIS IS A TempURTURE Amplitude ', maxTemp - minTemp);
+    
+}
+calcTempAmplitude(temperatures);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+console.log('PROBLEM 2:');
+
+const temperatures2 = [34, -2, -6, -19, 'error', 9, 13, 17, 15, 14, 9, 5000];
+
+let calcTempAmplitudeFrom2Arr = function(arr1, arr2){
+    let newArr = temperatures2.concat(temperatures);
+    console.log('new Arr', newArr);
+    calcTempAmplitude(newArr);
+
+
+}
+
+console.log(calcTempAmplitudeFrom2Arr(temperatures, temperatures2));
 
