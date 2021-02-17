e71aeeca219b6c9b2d7ce312f636920e3f307c2e
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-17 13:31:11 -0800

    Finish Projectt 2 - Operations with Classes. Open & close modal messages by clicking btn.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+console.log('openModel', btnOpenModal);
+
+let closeHandle = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+let openHandle = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  // console.log(btnOpenModal[i].textContent);
+  // 1. Open Modal when bttn cliecked
+
+  //    btnOpenModal[i].addEventListener('click', function () {
+  //     //    console.log('bttn clicked');
+  //        modal.classList.remove('hidden');
+  //        overlay.classList.remove('hidden');
+  //     });
+  // Make code above more DRY
+  btnOpenModal[i].addEventListener('click', openHandle);
+
+// 2. Close modal functionality
+overlay.addEventListener('click', closeHandle);
+btnCloseModal.addEventListener('click', closeHandle);
+
+document.addEventListener("keydown", function (e){
+    console.log(e.key);
+    if (e.key === "Escape" && !modal.classList.contains('hidden')){
+        closeHandle();
+    }
+})
