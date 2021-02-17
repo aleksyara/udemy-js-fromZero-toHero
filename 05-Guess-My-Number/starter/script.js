d55821f465caa90fd415f498ad03b9c09b700316
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-17 11:21:21 -0800

    refactoring code and add displayMessage function

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,15 +5,19 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+
+// Game Logic
 document.querySelector('.check').addEventListener('click', function () {
+  // parsing user entry
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   // when no imput
   if (!guess) {
-    document.querySelector('.message').textContent = 'NO Number!';
+    // document.querySelector('.message').textContent = 'NO Number!';
+    displayMessage('Invalid entry!');
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = 'green';
@@ -32,7 +36,7 @@ document.querySelector('.check').addEventListener('click', function () {
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost!';
+      displayMessage('You lost!');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -43,7 +47,7 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start Guessing ...';
+  displayMessage('Start Guessing ...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
   document.querySelector('.number').textContent = '?';
@@ -51,3 +55,9 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+
+// display message function 
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
