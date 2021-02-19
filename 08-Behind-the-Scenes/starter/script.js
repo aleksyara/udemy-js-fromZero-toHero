b5da4bfd8c9daf613f6c5894854591247bd70699
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-18 19:59:22 -0800

    .this word practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// console.log(this);
+
+// const calcAge = function(birthYear){
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+
+// calcAge(1981);
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear);
+    console.log(this);
+}
+
+calcAgeArrow(1981);
+
+const alex = {
+    year: 1981,
+    race: "white",
+    calcAge: function(){
+        console.log(this);
+        console.log(2037 - this.year);
+        
+    },
+}
+
+alex.calcAge();
+
+
+const ivan = {
+    year: 2018,
+};
+ivan.calcAge = alex.calcAge;
+ivan.calcAge();
